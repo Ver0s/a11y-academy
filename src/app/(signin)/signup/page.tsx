30a0233a2b77646c5201a990ad79199d3d404001
Arek <arek.stachyra41@gmail.com>
2025-07-19 13:11:45 +0200

    update tsc

--- src/app/(signin)/signup/page.tsx
+++ src/app/(signin)/signup/page.tsx
@@ -12,7 +12,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { useTransition } from "react";
-import { signUp } from "../(login)/actions";
+import { signUp } from "../actions";
 
 export default function SignUpPage() {
 	const [isPending, startTransition] = useTransition();

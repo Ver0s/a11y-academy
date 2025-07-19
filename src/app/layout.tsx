684ff78b73b7ac1bf78e76917791772c5ea00e90
Arek <arek.stachyra41@gmail.com>
2025-07-19 13:43:19 +0200

    v1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 	subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
+export const metadata: any = {
 	title: "a11y academy",
 	description: "Learn web accessibility with a11y academy",
 };
